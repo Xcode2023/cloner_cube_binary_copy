1518d63399f05c3e3cf0cc9097c71b723a70167a
1806120396@qq.com <1806120396@qq.com>
2023-09-09 23:00:25 +0800

    main

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+// app.load('../rabbit.spline');
+app.load('../molang_3_d_copy (2).spline');
+// app.load('https://prod.spline.design/GS-0O7st4BfEIq9D/scene.splinecode');
+
+
+console.log(typeof app);
+console.log(app._data);
+
